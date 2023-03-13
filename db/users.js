63f90cf800a353182150aa6b5b669b16c97256fc
Andrew Conlin <andrewpc1013@gmail.com>
2023-03-13 12:02:57 -0500

    made getUsers, getUserByUsername, getUserById

--- db/users.js
+++ db/users.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-catch */
 const client = require("./client");
 const bcrypt = require("bcrypt");
 
@@ -8,39 +9,75 @@ async function createUser({ username, password }) {
   const SALT_COUNT = 10;
 
   const hashedPassword = await bcrypt.hash(password, SALT_COUNT)
-  // eslint-disable-next-line no-useless-catch
-try {
-  const {
-    rows: [user],
-  } = await client.query(
-    `
-      INSERT INTO users (username, password)
-      VALUES ($1, $2)
-      ON CONFLICT (username) DO NOTHING
-      RETURNING *;
-      `,
-    [username, password]
-  );
 
-  return user;
-} catch (error) {
-  throw error
-}
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+        INSERT INTO users (username, hashedPassword)
+        VALUES ($1, $2)
+        ON CONFLICT (username) DO NOTHING
+        RETURNING *;
+        `,
+      [username, hashedPassword]
+    );
+
+    return user;
+  } catch (error) {
+    throw error
+  }
 }
 
 async function getUser({ username, password }) {
-  const user = await getUserByUserName(username);
+  const user = await getUserByUsername(username);
   const hashedPassword = user.password;
 
   const isValid = await bcrypt.compare(password, hashedPassword)
+
+  if (isValid) {
+    delete user.password;
+    return user;
+  }
 }
 
 async function getUserById(userId) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(`
+         SELECT id, username
+         FROM users
+         WHERE id=${userId};
+        `);
 
+    if (!user) {
+      return null;
+    }
+
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
-async function getUserByUsername(userName) {
+async function getUserByUsername(username) {
+  try {
+    const {
+      rows: [user],
+    } = await client.query(
+      `
+        SELECT *
+        FROM users
+        WHERE username=$1;
+        `,
+      [username]
+    );
 
+    return user;
+  } catch (error) {
+    throw error;
+  }
 }
 
 module.exports = {
