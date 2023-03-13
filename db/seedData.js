6c1c9e38021f1309a5097157e53f9f67c1858c32
Andrew Conlin <andrewpc1013@gmail.com>
2023-03-13 09:58:33 -0500

    created all tables and started on createUser db adapter function

--- db/seedData.js
+++ db/seedData.js
@@ -13,6 +13,9 @@ async function dropTables() {
     console.log("Dropping All Tables...");
 
     await client.query(`
+    DROP TABLE IF EXISTS routine_activities;
+    DROP TABLE IF EXISTS routines;
+    DROP TABLE IF EXISTS activities;
     DROP TABLE IF EXISTS users;
     `);
 
@@ -33,6 +36,35 @@ async function createTables() {
         id SERIAL PRIMARY KEY,
         username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL
+      );
+    `);
+
+    await client.query(`
+      CREATE TABLE activities (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        description TEXT NOT NULL
+      );
+    `);
+
+    await client.query(`
+      CREATE TABLE routines (
+        id SERIAL PRIMARY KEY,
+        "creatorId" INTEGER REFERENCES users(id),
+        "isPublic" BOOLEAN DEFAULT false,
+        name VARCHAR(255) UNIQUE NOT NULL,
+        goal TEXT NOT NULL
+      );
+    `);
+
+    await client.query(`
+      CREATE TABLE routine_activities (
+        id SERIAL PRIMARY KEY,
+        "routineId" INTEGER REFERENCES routines(id),
+        "activityId" INTEGER REFERENCES activities(id),
+        duration INTEGER,
+        count INTEGER,
+        UNIQUE("routineId", "activityId")
       );
     `);
 
