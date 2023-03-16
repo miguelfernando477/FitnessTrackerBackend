1d54b437c25458393b536a842437030f51c5879d
Miguel <miguelfernando477@gmail.com>
2023-03-16 10:03:50 -0500

    finished activities_api

--- api/activities.js
+++ api/activities.js
@@ -1,43 +1,72 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getAllActivities, createActivity, getActivityByName, getActivityById, updateActivity } = require("../db");
-const { requireUser } = require("./utils.js")
+const {
+  getAllActivities,
+  createActivity,
+  getActivityByName,
+  getActivityById,
+  updateActivity,
+  getPublicRoutinesByActivity,
+} = require("../db");
+const { requireUser } = require("./utils.js");
 const errorMessages = require("../errors.js");
 
 // GET /api/activities/:activityId/routines
+router.get("/:activityId/routines", async (req, res, next) => {
+  const { activityId } = req.params;
+
+  const activityExists = await getActivityById(activityId);
+  try {
+    if (!activityExists) {
+      res.status(401);
+      next({
+        message: errorMessages.ActivityNotFoundError(activityId),
+        name: "ActivityNotFound",
+      });
+    } else {
+      let publicRoutines = await getPublicRoutinesByActivity({
+        id: activityId,
+      });
+      res.send(publicRoutines);
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
 
 // GET /api/activities
 router.get("/", async (req, res, next) => {
-    try {
-        let activities = await getAllActivities()
-        res.send(activities)
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
-  })
-  
+  try {
+    let activities = await getAllActivities();
+    res.send(activities);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
 // POST /api/activities
 router.post("/", requireUser, async (req, res, next) => {
-    const { name, description } = req.body;
-    const _activity = await getActivityByName(name);
-    try {
-        if (_activity) {
-            res.status(401);
-            next({
-              message: errorMessages.ActivityExistsError(name),
-              name: "ActivityAlreadyExist",
-            });
-        }
-      const createdActivity = await createActivity({name, description});
-      if (createdActivity) {
-        res.send( createdActivity );
-      }
-    } catch ({ name, message }) {
-      next({ name, message });
+  const { name, description } = req.body;
+  const _activity = await getActivityByName(name);
+  try {
+    if (_activity) {
+      res.status(401);
+      next({
+        message: errorMessages.ActivityExistsError(name),
+        name: "ActivityAlreadyExist",
+      });
     }
-  });
-  
+    const createdActivity = await createActivity({ name, description });
+    if (createdActivity) {
+      res.send(createdActivity);
+    }
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
+
 // PATCH /api/activities/:activityId
 router.patch("/:activityId", requireUser, async (req, res, next) => {
   const { activityId } = req.params;
@@ -59,20 +88,23 @@ router.patch("/:activityId", requireUser, async (req, res, next) => {
     const _activity = await getActivityByName(name);
 
     if (!activityExists) {
-      res.status(401)
+      res.status(401);
       next({
         message: errorMessages.ActivityNotFoundError(activityId),
         name: "ActivityNotFound",
       });
     } else if (_activity) {
-        res.status(401);
-        next({
-          message: errorMessages.ActivityExistsError(name),
-          name: "ActivityAlreadyExist",
-        });
+      res.status(401);
+      next({
+        message: errorMessages.ActivityExistsError(name),
+        name: "ActivityAlreadyExist",
+      });
     } else {
-      const updatedActivity = await updateActivity({id:activityId, ...updateFields});
-      res.send( updatedActivity );
+      const updatedActivity = await updateActivity({
+        id: activityId,
+        ...updateFields,
+      });
+      res.send(updatedActivity);
     }
   } catch ({ name, message }) {
     next({ name, message });
